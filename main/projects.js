064fffd8322fc87ab611298718915dc5ed54215e
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-03-07 10:07:17 +0100

    Create projects.js

---
+++ main/projects.js
@@ -0,0 +1,63 @@
+const projects = [
+  
+  {
+    "id": 0,
+    "title": "D3usN0tam",
+    "img": "/projects/D3usN0tam/sitecontent/avatar.jpg",
+    "overlay": "/projects/D3usN0tam/sitecontent/overlay.png",
+    "outline": "#ffffff",
+    "content":
+      [
+        {"h4-super": "Защита вашего контента"},
+        {"img-super": "/projects/D3usN0tam/sitecontent/img1.jpg"},
+        {"p-title": "Тут текст"},
+        {"img-super": "/projects/D3usN0tam/sitecontent/img2.jpg"},
+        {"img-super": "/projects/D3usN0tam/sitecontent/img3.jpg"},
+        {"p-title": "Ещё один текст."},
+        {"p-title": "Последний текст."}
+      ],
+    "hash": "d3usn0tam",
+    "url": "https://deusnotam.github.io"
+  },
+
+  {
+    "id": 1,
+    "title": "111",
+    "img": "/projects/111/sitecontent/avatar.jpg",
+    "overlay": "/projects/111/sitecontent/overlay.png",
+    "outline": "#ddd",
+    "content":
+      [
+        {"h4-super": "111h4"},
+        {"img-super": "/projects/111/sitecontent/img1.jpg"},
+        {"p-title": "Тут текст"},
+        {"img-super": "/projects/111/sitecontent/img2.jpg"},
+        {"img-super": "/projects/111/sitecontent/img3.jpg"},
+        {"p-title": "Ещё один текст."},
+        {"p-title": "Последний текст."}
+      ],
+    "hash": "111",
+    "url": "https://111.me"
+  },
+
+  {
+    "id": 2,
+    "title": "projects222",
+    "img": "/projects/222/sitecontent/avatar.jpg",
+    "overlay": "/projects/222/sitecontent/overlay.png",
+    "outline": "#000",
+    "content":
+      [
+        {"h4-super": "222titleform"},
+        {"img-super": "/projects/222/sitecontent/img1.jpg"},
+        {"p-title": "Тут текст"},
+        {"img-super": "/projects/222/sitecontent/img2.jpg"},
+        {"img-super": "/projects/222/sitecontent/img3.jpg"},
+        {"p-title": "Ещё один текст."},
+        {"p-title": "Последний текст."}
+      ],
+    "hash": "222",
+    "url": "https://abros.me"
+  },
+                            
+];

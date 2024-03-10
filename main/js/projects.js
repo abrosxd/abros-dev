9eb2ab279fcc2ba84db938424cf1e60da96e12db
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-03-10 19:43:47 +0100

    Add files via upload

---
+++ main/js/projects.js
@@ -0,0 +1,23 @@
+const projects = [
+  
+    {
+      "id": 0,
+      "title": "DEUS",
+      "img": "main/projects/DEUS/avatar.jpg",
+      "overlay": "main/projects/DEUS/overlay.png",
+      "outline": "#ffffff",
+      "content-ru":
+        [
+          {"h4-super": "Защита вашего контента"},
+          {"img-super": "main/projects/DEUS/avatar.jpg"},
+          {"p-title": "Тут текст"},
+          {"img-super": "main/projects/DEUS/avatar.jpg"},
+          {"img-super": "main/projects/DEUS/avatar.jpg"},
+          {"p-title": "Ещё один текст."},
+          {"p-title": "Последний текст."}
+        ],
+      "hash": "deus",
+      "url": "https://deusnotam.github.io"
+    },
+                              
+  ];

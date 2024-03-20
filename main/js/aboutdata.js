c787aeaa29794a1ce595056af3f167e639ca82e9
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-03-20 19:49:13 +0100

    Update

--- main/js/aboutdata.js
+++ main/js/aboutdata.js
@@ -1,27 +1,39 @@
 const notes = [
   
   {
-    "link": "#",
-    "titleRU": "Новости",
-    "textRU": "Ну то, что вы видите этот сайт - можно считать новостью 🤗",
-    "color": "rgb(72, 154, 255)",
-    "img": "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbTM0cHlobXc1aDVkN2ptaWhlODM0eXV0cW1tdXBmbXdza3VlMjV2NyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/kfqjGTVX3YE6Y/giphy.gif"
+    link: "#",
+    title: {
+      RU: "Новости",
+    },
+    text: {
+      RU: "Ну то, что вы видите этот сайт - можно считать новостью 🤗",
+    },
+    color: "rgb(72, 154, 255)",
+    img: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbTM0cHlobXc1aDVkN2ptaWhlODM0eXV0cW1tdXBmbXdza3VlMjV2NyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/kfqjGTVX3YE6Y/giphy.gif",
   },
 
   {
-    "link": "#",
-    "titleRU": "Курсы",
-    "textRU": "Ничего нет - расслабтесь 😅",
-    "color": "azure",
-    "img": "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOTkxcXFtajBnZnFrNzUzdmhqdWU5anUyNnB0aHVzdHpuZ25oNWU5ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/SwOyrG5xAmOI1pv0Rf/giphy.gif"
+    link: "#",
+    title: {
+      RU: "Курсы",
+    },
+    text: {
+      RU: "Ничего нет - расслабтесь 😅",
+    },
+    color: "azure",
+    img: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOTkxcXFtajBnZnFrNzUzdmhqdWU5anUyNnB0aHVzdHpuZ25oNWU5ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/SwOyrG5xAmOI1pv0Rf/giphy.gif",
   },
 
   {
-    "link": "#",
-    "titleRU": "Крипта",
-    "textRU": "Близится халвинг. Ваши прогнозы? 🤔",
-    "color": "rgb(110, 68, 0)",
-    "img": "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGRza3pxNW0zcmV1dHh1NXQ5c21oNm9paXJ2aXQxcXZybzQzaGhqNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/oNFP9kltPi7fp8TUAV/giphy.gif"
+    link: "#",
+    title: {
+      RU: "Крипта",
+    },
+    text: {
+      RU: "Близится халвинг. Ваши прогнозы? 🤔",
+    },
+    color: "rgb(110, 68, 0)",
+    img: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGRza3pxNW0zcmV1dHh1NXQ5c21oNm9paXJ2aXQxcXZybzQzaGhqNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/oNFP9kltPi7fp8TUAV/giphy.gif",
   },
 
 ];
@@ -29,18 +41,30 @@ const notes = [
 const faq = [
   
   {
-    "titleRU": "Тильда — хорошее решение?",
-    "textRU": "Тильда — отличный выбор, для небольших проектов или для больших, но с бюджетом. Почему именно так? Все дело в том, что Тильда это конструктор сайтов, за счет гибкости платформы после разработки сайта вы сможете даже сами корректировать тексты и менять картинки. Но придется мирится с тем, что у неё есть или покупать подписки и платить за дополнительную разработку. В любом случае это до сих пор дешевле чем разработка сайта с нуля."
+    title: {
+      RU: "Тильда — хорошее решение?",
+    },
+    text: {
+      RU: "Тильда — отличный выбор, для небольших проектов или для больших, но с бюджетом. Почему именно так? Все дело в том, что Тильда это конструктор сайтов, за счет гибкости платформы после разработки сайта вы сможете даже сами корректировать тексты и менять картинки. Но придется мирится с тем, что у неё есть или покупать подписки и платить за дополнительную разработку. В любом случае это до сих пор дешевле чем разработка сайта с нуля.",
+    },
   },
 
   {
-    "titleRU": "Почему такая цена?",
-    "textRU": "Сделать сайт, написать модификацию, улучшить модификацию — не так просто и быстро как кажется. Процесс разработки включает множество неявных этапов, которые напрямую влияют на результат. Разработка - процесс тонкий и кропотливый. В ходе работы зачастую происходят правки, которые увеличивают сложность и время работы над проектом. Это долгая работа, которая требует определенных навыков, ради которых я постоянно прохожу обучение, поэтому стоимость за разработку не может быть низкой."
+    title: {
+      RU: "Почему такая цена?",
+    },
+    text: {
+      RU: "Сделать сайт, написать модификацию, улучшить модификацию — не так просто и быстро как кажется. Процесс разработки включает множество неявных этапов, которые напрямую влияют на результат. Разработка - процесс тонкий и кропотливый. В ходе работы зачастую происходят правки, которые увеличивают сложность и время работы над проектом. Это долгая работа, которая требует определенных навыков, ради которых я постоянно прохожу обучение, поэтому стоимость за разработку не может быть низкой.",
+    },
   },
 
   {
-    "titleRU": "В чем отличие правок от редакций?",
-    "textRU": "Правки - это когда вы просите внести мелкие изменения: поменять цвет, передвинуть кнопку, скруглить края...<br><br>Редакции - это когда вы просите внести крупные изменения: поменять функционал, изменить дизайн блока, сделать новый блок..."
+    title: {
+      RU: "В чем отличие правок от редакций?",
+    },
+    text: {
+      RU: "Правки - это когда вы просите внести мелкие изменения: поменять цвет, передвинуть кнопку, скруглить края...<br><br>Редакции - это когда вы просите внести крупные изменения: поменять функционал, изменить дизайн блока, сделать новый блок...",
+    },
   },
 
 ];
@@ -48,18 +72,24 @@ const faq = [
 const facts = [
   
   {
-    "img": "https://avatars.githubusercontent.com/u/50206778?s=200&v=4",
-    "textRU": "Если кто-то из вас когда-то жаловался, что Airtable платный. То я нашел для вас Nocodb, обязательно погуглите!"
+    img: "https://avatars.githubusercontent.com/u/50206778?s=200&v=4",
+    text: {
+      RU: "Если кто-то из вас когда-то жаловался, что Airtable платный. То я нашел для вас Nocodb, обязательно погуглите!",
+    },
   },
 
   {
-    "img": "https://deusnotam.github.io/site/img/logo.jpeg",
-    "textRU": "Боитесь, что заказчик не оплатит? А может хотите защитится от этого на всякий случай? Тогда обязательно загляни в проект DEUS."
+    img: "https://deusnotam.github.io/site/img/logo.jpeg",
+    text: {
+      RU: "Боитесь, что заказчик не оплатит? А может хотите защитится от этого на всякий случай? Тогда обязательно загляни в проект DEUS.",
+    },
   },
 
   {
-    "img": "main/img/facts/music.gif",
-    "textRU": "А вы знали, что по сайту можно путешествовать с приятной музыкой? Просто кликни по иконке с волной 😉"
+    img: "main/img/facts/music.gif",
+    text: {
+      RU: "А вы знали, что по сайту можно путешествовать с приятной музыкой? Просто кликни по иконке с волной 😉",
+    },
   },
 
 ];
@@ -67,9 +97,13 @@ const facts = [
 const reviews = [
   
   {
-    "img": "https://upload.wikimedia.org/wikipedia/en/d/d0/Dogecoin_Logo.png",
-    "nameRU": "Доги Коинов",
-    "textRU": "Советую вам оставлять отзывы, это показывает мастерство хозяина"
+    img: "https://upload.wikimedia.org/wikipedia/en/d/d0/Dogecoin_Logo.png",
+    name: {
+      RU: "Доги Коинов",
+    },
+    text: {
+      RU: "Советую вам оставлять отзывы, это показывает мастерство хозяина",
+    },
   },
 
 ];
@@ -77,24 +111,30 @@ const reviews = [
 const soft = [
   
   {
-    "title": "Visual Studio Code",
-    "href": "https://code.visualstudio.com",
-    "img": "https://yt3.googleusercontent.com/_q52i8bUAEvcb7JR4e-eNTv23y2A_wg5sCz0NC0GrGtcw1CRMWJSOPVHUDh_bngD0q4gMvVeoA=s900-c-k-c0x00ffffff-no-rj",
-    "textRU": "Visual Studio Code — редактор исходного кода, разработанный Microsoft для Windows, Linux и macOS."
+    title: "Visual Studio Code",
+    href: "https://code.visualstudio.com",
+    img: "https://yt3.googleusercontent.com/_q52i8bUAEvcb7JR4e-eNTv23y2A_wg5sCz0NC0GrGtcw1CRMWJSOPVHUDh_bngD0q4gMvVeoA=s900-c-k-c0x00ffffff-no-rj",
+    text: {
+      RU: "Visual Studio Code — редактор исходного кода, разработанный Microsoft для Windows, Linux и macOS.",
+    },
   },
 
   {
-    "title": "Adobe Creative Cloud",
-    "href": "https://creativecloud.adobe.com",
-    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Adobe_Creative_Cloud_rainbow_icon.svg/1200px-Adobe_Creative_Cloud_rainbow_icon.svg.png",
-    "textRU": "Photoshop, Illustrator, Premiere Pro, After Effects и многое другое в одной подписке Adobe Creative Cloud."
+    title: "Adobe Creative Cloud",
+    href: "https://creativecloud.adobe.com",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Adobe_Creative_Cloud_rainbow_icon.svg/1200px-Adobe_Creative_Cloud_rainbow_icon.svg.png",
+    text: {
+      RU: "Photoshop, Illustrator, Premiere Pro, After Effects и многое другое в одной подписке Adobe Creative Cloud.",
+    },
   },
 
   {
-    "title": "ICONS 8",
-    "href": "https://icons8.com",
-    "img": "https://maxst.icons8.com/vue-static/icon/menu/logo.svg",
-    "textRU": "Сайт с иконками, иллюстрациями, фотографиями и 3D моделями для ваших проектов."
+    title: "ICONS 8",
+    href: "https://icons8.com",
+    img: "https://maxst.icons8.com/vue-static/icon/menu/logo.svg",
+    text: {
+      RU: "Сайт с иконками, иллюстрациями, фотографиями и 3D моделями для ваших проектов.",
+    },
   },
 
 ];
@@ -102,10 +142,12 @@ const soft = [
 const lessons = [
   
   {
-    "title": "Dogecoin",
-    "href": "#",
-    "img": "https://upload.wikimedia.org/wikipedia/en/d/d0/Dogecoin_Logo.png",
-    "textRU": "Скоро тут появятся уроки, а пока вот вам Dogecoin."
+    title: "Dogecoin",
+    href: "#",
+    img: "https://upload.wikimedia.org/wikipedia/en/d/d0/Dogecoin_Logo.png",
+    text: {
+      RU: "Скоро тут появятся уроки, а пока вот вам Dogecoin.",
+    },
   },
 
 ];
@@ -113,23 +155,23 @@ const lessons = [
 const gallery = [
   
   {
-    "img": "main/img/gallery/1.jpeg",
+    img: "main/img/gallery/1.jpeg",
   },
 
   {
-    "img": "main/img/gallery/2.jpeg",
+    img: "main/img/gallery/2.jpeg",
   },
 
   {
-    "img": "main/img/gallery/3.png",
+    img: "main/img/gallery/3.png",
   },
 
   {
-    "img": "main/img/gallery/4.png",
+    img: "main/img/gallery/4.png",
   },
 
   {
-    "img": "main/img/gallery/5.png",
+    img: "main/img/gallery/5.png",
   },
 
 ];
@@ -137,57 +179,57 @@ const gallery = [
 const stack = [
   
   {
-    "title": "Tilda",
-    "color": "#000000",
-    "bgcolor": "#ffb20080"
+    title: "Tilda",
+    color: "#000000",
+    bgcolor: "#ffb20080",
   },
 
   {
-    "title": "VS Code",
-    "color": "#2532eb",
-    "bgcolor": "#ffffff80"
+    title: "VS Code",
+    color: "#2532eb",
+    bgcolor: "#ffffff80",
   },
 
   {
-    "title": "Figma",
-    "color": "#25eb8f",
-    "bgcolor": "#00000080"
+    title: "Figma",
+    color: "#25eb8f",
+    bgcolor: "#00000080",
   },
 
   {
-    "title": "NocoDB",
-    "color": "#ffffff",
-    "bgcolor": "#0015ff80"
+    title: "NocoDB",
+    color: "#ffffff",
+    bgcolor: "#0015ff80",
   },
 
   {
     "title": "HTML",
     "color": "#ffffff",
-    "bgcolor": "#ff590080"
+    "bgcolor": "#ff590080",
   },
 
   {
     "title": "JavaScript",
     "color": "#000000",
-    "bgcolor": "#f6ff0080"
+    "bgcolor": "#f6ff0080",
   },
 
   {
-    "title": "CSS",
-    "color": "#ffffff",
-    "bgcolor": "#0015ff80"
+    title: "CSS",
+    color: "#ffffff",
+    bgcolor: "#0015ff80",
   },
 
   {
-    "title": "Photoshop",
-    "color": "#32AAFF",
-    "bgcolor": "#001E3680"
+    title: "Photoshop",
+    color: "#32AAFF",
+    bgcolor: "#001E3680",
   },
 
   {
-    "title": "Illustrator",
-    "color": "#FF9B00",
-    "bgcolor": "#32000080"
+    title: "Illustrator",
+    color: "#FF9B00",
+    bgcolor: "#32000080",
   },
 
 
@@ -195,39 +237,57 @@ const stack = [
 
 const coinData = {
   BTC: {
-    textRU: 'Отправляйте только активы <span style="color: #fff; background-color: rgb(255 147 0 / 60%); border-radius: 10px; padding: 0 6px; font-weight: 600;">Bitcoin(BTC)</span> на этот адрес. Остальные активы будут безвозвратно утеряны.',
-    address: 'bc1qrpmcy4zz7g6rtfxka8j4nk3yka72kmacvmam0a'
+    text: {
+      RU: 'Отправляйте только активы <span style="color: #fff; background-color: rgb(255 147 0 / 60%); border-radius: 10px; padding: 0 6px; font-weight: 600;">Bitcoin(BTC)</span> на этот адрес. Остальные активы будут безвозвратно утеряны.',
+    },
+    address: 'bc1qrpmcy4zz7g6rtfxka8j4nk3yka72kmacvmam0a',
   },
   ETH: {
-    textRU: 'Отправляйте только <span style="color: #fff; background-color: rgb(141 176 255 / 50%); border-radius: 10px; padding: 0 6px; font-weight: 600;">Ethereum(ETH) или активы в сети Ethereum(ERC20)</span> на этот адрес. Остальные активы будут безвозвратно утеряны.',
-    address: '0x96800af5023619eA338551a8E20b612C8Dd6192C'
+    text: {
+      RU: 'Отправляйте только <span style="color: #fff; background-color: rgb(141 176 255 / 50%); border-radius: 10px; padding: 0 6px; font-weight: 600;">Ethereum(ETH) или активы в сети Ethereum(ERC20)</span> на этот адрес. Остальные активы будут безвозвратно утеряны.',
+    },
+    address: '0x96800af5023619eA338551a8E20b612C8Dd6192C',
   },
   XRP: {
-    textRU: 'Отправляйте только активы <span style="color: #fff; background-color: rgb(0 0 0 / 50%); border-radius: 10px; padding: 0 6px; font-weight: 600;">XRP(XRP)</span> на этот адрес. Остальные активы будут безвозвратно утеряны.',
-    address: 'raHW9xPXB3YiQqkraR2DT1oH8vUr7X5EQT'
+    text: {
+      RU: 'Отправляйте только активы <span style="color: #fff; background-color: rgb(0 0 0 / 50%); border-radius: 10px; padding: 0 6px; font-weight: 600;">XRP(XRP)</span> на этот адрес. Остальные активы будут безвозвратно утеряны.',
+    },
+    address: 'raHW9xPXB3YiQqkraR2DT1oH8vUr7X5EQT',
   },
   TON: {
-    textRU: 'Отправляйте только активы <span style="color: #fff; background-color: rgb(0 78 255 / 50%); border-radius: 10px; padding: 0 6px; font-weight: 600;">TON(TON)</span> на этот адрес. Остальные активы будут безвозвратно утеряны.',
-    address: 'EQAmDD_sRSt-LuVtw9wBfxq8v7eCH-XP3oA8wQuQ5s2EN_wB'
+    text: {
+      RU: 'Отправляйте только активы <span style="color: #fff; background-color: rgb(0 78 255 / 50%); border-radius: 10px; padding: 0 6px; font-weight: 600;">TON(TON)</span> на этот адрес. Остальные активы будут безвозвратно утеряны.',
+    },
+    address: 'EQAmDD_sRSt-LuVtw9wBfxq8v7eCH-XP3oA8wQuQ5s2EN_wB',
   },
   TRX: {
-    textRU: 'Отправляйте только <span style="color: #fff; background-color: rgb(255 0 0 / 60%); border-radius: 10px; padding: 0 6px; font-weight: 600;">Tron(TRX) или активы в сети Tron(TRС20)</span> на этот адрес. Остальные активы будут безвозвратно утеряны.',
-    address: 'TRD7vypAmXBP5qmXeejGUMXjgUMAMuS3mY'
+    text: {
+      RU: 'Отправляйте только <span style="color: #fff; background-color: rgb(255 0 0 / 60%); border-radius: 10px; padding: 0 6px; font-weight: 600;">Tron(TRX) или активы в сети Tron(TRС20)</span> на этот адрес. Остальные активы будут безвозвратно утеряны.',
+    },
+    address: 'TRD7vypAmXBP5qmXeejGUMXjgUMAMuS3mY',
   },
   SOL: {
-    textRU: 'Отправляйте только <span style="color: #acffec; background-color: rgb(171 0 255 / 50%); border-radius: 10px; padding: 0 6px; font-weight: 600;">Solana(SOL) или активы в сети Solana(SPL)</span> на этот адрес. Остальные активы будут безвозвратно утеряны.',
-    address: '8y1QfdDF4XGEWNM5Aj7N19c93MT31kjn3Cu3p5V7YhnX'
+    text: {
+      RU: 'Отправляйте только <span style="color: #acffec; background-color: rgb(171 0 255 / 50%); border-radius: 10px; padding: 0 6px; font-weight: 600;">Solana(SOL) или активы в сети Solana(SPL)</span> на этот адрес. Остальные активы будут безвозвратно утеряны.',
+    },
+    address: '8y1QfdDF4XGEWNM5Aj7N19c93MT31kjn3Cu3p5V7YhnX',
   },
   LTC: {
-    textRU: 'Отправляйте только активы <span style="color: #fff; background-color: rgb(0 47 178 / 50%); border-radius: 10px; padding: 0 6px; font-weight: 600;">Litecoin(LTC)</span> на этот адрес. Остальные активы будут безвозвратно утеряны.',
-    address: 'ltc1qrrv5lx0epj37qnp2ltyhrdq6srlky4typzy026'
+    text: {
+      RU: 'Отправляйте только активы <span style="color: #fff; background-color: rgb(0 47 178 / 50%); border-radius: 10px; padding: 0 6px; font-weight: 600;">Litecoin(LTC)</span> на этот адрес. Остальные активы будут безвозвратно утеряны.',
+    },
+    address: 'ltc1qrrv5lx0epj37qnp2ltyhrdq6srlky4typzy026',
   },
   BCH: {
-    textRU: 'Отправляйте только активы <span style="color: #fff; background-color: rgb(16 255 0 / 50%); border-radius: 10px; padding: 0 6px; font-weight: 600;">Bitcoin_Cash(BCH)</span> на этот адрес. Остальные активы будут безвозвратно утеряны.',
-    address: 'qq8mpv5fkse5062t7sn0al8545x4lnx03u2wxr4k22'
+    text: {
+      RU: 'Отправляйте только активы <span style="color: #fff; background-color: rgb(16 255 0 / 50%); border-radius: 10px; padding: 0 6px; font-weight: 600;">Bitcoin_Cash(BCH)</span> на этот адрес. Остальные активы будут безвозвратно утеряны.',
+    },
+    address: 'qq8mpv5fkse5062t7sn0al8545x4lnx03u2wxr4k22',
   },
   DOGE: {
-    textRU: 'Отправляйте только активы <span style="color: #fff; background-color: rgb(255 215 0 / 50%); border-radius: 10px; padding: 0 6px; font-weight: 600;">Dogecoin(DOGE)</span> на этот адрес. Остальные активы будут безвозвратно утеряны.',
-    address: 'DMHtna8VCEB1BUeP2a9ki9TkiFSDMxtbo1'
+    text: {
+      RU: 'Отправляйте только активы <span style="color: #fff; background-color: rgb(255 215 0 / 50%); border-radius: 10px; padding: 0 6px; font-weight: 600;">Dogecoin(DOGE)</span> на этот адрес. Остальные активы будут безвозвратно утеряны.',
+    },
+    address: 'DMHtna8VCEB1BUeP2a9ki9TkiFSDMxtbo1',
   },
 };

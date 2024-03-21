2ceecf700f1eded5ab258f6b1ddeb0367b898959
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-03-21 09:33:52 +0100

    Update langdata.js

--- main/js/langdata.js
+++ main/js/langdata.js
@@ -1,135 +1,129 @@
 const menu = [
-    {
-        portfolio: {
-            RU: "Портфолио",
-            EN: "Portfolio",
-            PL: "Portfolio",
-        },
+  {
+    portfolio: {
+      RU: "Портфолио",
+      EN: "Portfolio",
+      PL: "Portfolio",
     },
-
-    {
-        projects: {
-            RU: "Проекты",
-            EN: "Projects",
-            PL: "Projektowanie",
-        },
+  },
+  {
+    projects: {
+      RU: "Проекты",
+      EN: "Projects",
+      PL: "Projektowanie",
     },
-
-    {
-        about: {
-            RU: "Обо мне",
-            EN: "About me",
-            PL: "O mnie",
-        },
+  },
+  {
+    about: {
+      RU: "Обо мне",
+      EN: "About me",
+      PL: "O mnie",
     },
-]
+  },
+];
 
 const aboutinfome = {
-    RU: "👋 Хай — Я Данил",
-    EN: "👋 Hi - I'm Daniel",
-    PL: "👋 Cześć - jestem Daniel",
-}
+  RU: "👋 Хай — Я Данил, веб-дизайнер и веб-разработчик. Здесь ты найдешь всю нужную информацию обо мне и даже больше. Добро пожаловать!",
+  EN: "👋 Hi - I'm Daniel, a web designer and web developer. Here you'll find all the necessary information about me and even more. Welcome!",
+  PL: "👋 Cześć - jestem Daniel, web designer i web developer. Tutaj znajdziesz wszystkie potrzebne informacje o mnie i nawet więcej. Witaj!",
+};
+
 
 const aboutinfotime = [
-    {
-        title: {
-            RU: "Мое время",
-            EN: "My time",
-            PL: "Mój czas",
-        },
+  {
+    title: {
+      RU: "Мое время",
+      EN: "My time",
+      PL: "Mój czas",
     },
-
-    {
-        days: {
-            RU: ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'],
-            EN: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-            PL: ['Nd', 'Pn', 'Wt', 'Śr', 'Czw', 'Pt', 'Sob'],
-        },
+  },
+  {
+    days: {
+      RU: ["ВС", "ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ"],
+      EN: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+      PL: ["Nd", "Pn", "Wt", "Śr", "Czw", "Pt", "Sob"],
     },
-]
+  },
+];
 
 const aboutinfopay = [
-    {
-        title: {
-            RU: "Донаты и реквизиты",
-            EN: "Donations and details",
-            PL: "Darowizny i szczegóły",
-        },
+  {
+    title: {
+      RU: "Донаты и реквизиты",
+      EN: "Donations and details",
+      PL: "Darowizny i szczegóły",
     },
-
-    {
-        button: {
-            RU: "Скопировать",
-            EN: "Copy",
-            PL: "Kopiuj",
-        },
+  },
+  {
+    button: {
+      RU: "Скопировать",
+      EN: "Copy",
+      PL: "Kopiuj",
     },
-
-    {
-        buttonclick: {
-            RU: "Скопировано",
-            EN: "Copied",
-            PL: "Skopiowano",
-        },
+  },
+  {
+    buttonclick: {
+      RU: "Скопировано",
+      EN: "Copied",
+      PL: "Skopiowano",
     },
-]
+  },
+];
 
 const aboutinforeviews = {
-    RU: "Отзывы",
-    EN: "Reviews",
-    PL: "Opinie",
-}
+  RU: "Отзывы",
+  EN: "Reviews",
+  PL: "Opinie",
+};
 
 const aboutinfofaq = {
-    RU: "Ответы на вопросы",
-    EN: "Answers on questions",
-    PL: "Odpowiedzi na pytania",
-}
+  RU: "Ответы на вопросы",
+  EN: "Answers on questions",
+  PL: "Odpowiedzi na pytania",
+};
 
 const aboutinfogallery = {
-    RU: "Фотоальбом",
-    EN: "Photo album",
-    PL: "Album zdjęć",
-}
+  RU: "Фотоальбом",
+  EN: "Photo album",
+  PL: "Album zdjęć",
+};
 
 const aboutinfostack = {
-    RU: "Стек",
-    EN: "Stack",
-    PL: "Stos",
-}
+  RU: "Стек",
+  EN: "Stack",
+  PL: "Stos",
+};
 
 const aboutinforandomfacts = [
-    {
-        title: {
-            RU: "Случайный факт",
-            EN: "Random fact",
-            PL: "Losowy fakt",
-        },
+  {
+    title: {
+      RU: "Случайный факт",
+      EN: "Random fact",
+      PL: "Losowy fakt",
     },
-
-    {
-        button: {
-            RU: "Другой факт",
-            EN: "Another fact",
-            PL: "Kolejny fakt",
-        },
+  },
+  {
+    button: {
+      RU: "Другой факт",
+      EN: "Another fact",
+      PL: "Kolejny fakt",
     },
-]
+  },
+];
 
 const aboutinfotools = [
-    {
-        soft: {
-            RU: "Софт",
-            EN: "Soft",
-            PL: "Soft",
-        },
+  {
+    soft: {
+      RU: "Софт",
+      EN: "Soft",
+      PL: "Soft",
     },
-
-    {
-        materials: {
-            RU: "Материалы",
-            EN: "Materials",
-            PL: "Materiały",
-        },
+  },
+  {
+    materials: {
+      RU: "Материалы",
+      EN: "Materials",
+      PL: "Materiały",
     },
-]
+  },
+];

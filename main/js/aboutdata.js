a578f1c8bb57ac3fbcca5c945b111819c86ec250
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-03-24 19:40:15 +0100

    Update

--- main/js/aboutdata.js
+++ main/js/aboutdata.js
@@ -227,54 +227,63 @@ const stack = [
   
   {
     title: "Tilda",
+    img: "https://upload.wikimedia.org/wikipedia/commons/d/d6/Tilda_Logo.png",
     color: "#000000",
     bgcolor: "#ffb20080",
   },
 
   {
     title: "VS Code",
+    img: "https://yt3.googleusercontent.com/_q52i8bUAEvcb7JR4e-eNTv23y2A_wg5sCz0NC0GrGtcw1CRMWJSOPVHUDh_bngD0q4gMvVeoA=s900-c-k-c0x00ffffff-no-rj",
     color: "#2532eb",
     bgcolor: "#ffffff80",
   },
 
   {
     title: "Figma",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Figma-logo.svg/1667px-Figma-logo.svg.png",
     color: "#25eb8f",
     bgcolor: "#00000080",
   },
 
   {
     title: "NocoDB",
+    img: "https://avatars.githubusercontent.com/u/50206778?s=280&v=4",
     color: "#ffffff",
     bgcolor: "#0015ff80",
   },
 
   {
     title: "HTML",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png",
     color: "#ffffff",
     bgcolor: "#ff590080",
   },
 
   {
     title: "JavaScript",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
     color: "#000000",
     bgcolor: "#f6ff0080",
   },
 
   {
     title: "CSS",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png",
     color: "#ffffff",
     bgcolor: "#0015ff80",
   },
 
   {
     title: "Photoshop",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Adobe_Photoshop_CC_icon.svg/2101px-Adobe_Photoshop_CC_icon.svg.png",
     color: "#32AAFF",
     bgcolor: "#001E3680",
   },
 
   {
     title: "Illustrator",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Adobe_Illustrator_CC_icon.svg/2101px-Adobe_Illustrator_CC_icon.svg.png",
     color: "#FF9B00",
     bgcolor: "#32000080",
   },

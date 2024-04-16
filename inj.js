f6acf7e40088817e66db5cd01f6bcd65f38cbffa
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-04-16 10:32:21 +0200

    Upd

---
+++ inj.js
@@ -0,0 +1,17 @@
+/*!
+ * Injector.js v1.0.0a
+ * (c) 2024-2024
+ * by Daniel Abros
+ * Site → https://abros.me
+ * Telegram → https://t.me/abrosxd
+ * Инжектор для подключения сторонних скриптов одной строкой
+ * Используется для быстрой разработки
+ */
+
+loadScript('https://deusnotam.github.io/D3US/d3us.js');
+
+function loadScript(src) {
+  const script = document.createElement('script');
+  script.src = src;
+  document.head.appendChild(script);
+}

4a14b237e6cc92c712075ea93e7802ec44800f72
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-04-17 10:36:35 +0200

    Upd

--- main/js/lang.js
+++ main/js/lang.js
@@ -1,45 +1,28 @@
-function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+function setLanguageInLocalStorage(language) {
+    localStorage.setItem("Language", language);
 }
-        
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
+
+function getLanguageFromLocalStorage() {
+    return localStorage.getItem("Language");
 }
-        
-function eraseCookie(name) {
-    document.cookie = name + '=; Max-Age=-99999999;';
+
+function eraseLanguageFromLocalStorage() {
+    localStorage.removeItem("Language");
 }
-        
-function reloadPage() {
-    window.location.reload();
-}
-        
-var savedLanguage = getCookie("currentLanguage");
+
+var savedLanguage = getLanguageFromLocalStorage();
 
 if (!savedLanguage) {
-    setCookie("currentLanguage", currentLanguage, 30);
+    setLanguageInLocalStorage("EN");
 }
-        
+
 var currentLanguage = savedLanguage || "EN";
-        
+
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.submenu .button').forEach(function(item) {
         item.addEventListener('click', function() {
             var currentLanguage = this.getAttribute('lang');
-            setCookie("currentLanguage", currentLanguage, 30);
+            setLanguageInLocalStorage(currentLanguage);
             reloadPage();
         });
     });

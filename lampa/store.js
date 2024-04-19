b123a3da7a18b3f568a46186486da3f36a4c8c1f
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-04-19 04:19:10 +0200

    Upd

--- lampa/store.js
+++ lampa/store.js
@@ -85,7 +85,9 @@
                     var cardHTML = '<div class="adAbrosstore__card">' +
                                     '<img class="adAbrosstore__card__image" style="border-radius: 1em;" src="' + item.image + '">' +
                                     '<div class="adAbrosstore__card__author" style="position: absolute; top: 0; margin: 4px; font-size: 1.4em; color:' + item.colorauthor + ' ">' + item.author + '</div>' +
+                                    '<div class="adAbrosstore__card__content">' +
                                     '<div class="adAbrosstore__card__text" style="font-size: 1.4em; color:' + item.colortext + '">' + item.text + '</div>' +
+                                    '</div>' +
                                     '</div>';
 
                     adAbrosStoreBody.insertAdjacentHTML('beforeend', cardHTML);

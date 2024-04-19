420a92e58352d70f5e3c435b0a5f4388caac91f6
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-04-19 03:16:28 +0200

    upd

--- lampa/store.js
+++ lampa/store.js
@@ -93,6 +93,15 @@
                 });
             })
             .catch(error => console.error('Ошибка загрузки данных:', error));
+
+            $('.adAbrosStore__body').slick({
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 5000,
+                arrows: false,
+            });
         });
     }
 

46630635befaeb52e13ae31c4f8d5bf0d0c3feef
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-04-23 11:40:28 +0200

    upd

--- lampa/store.js
+++ lampa/store.js
@@ -394,16 +394,7 @@ function showReload(reloadText){
             },
             onRender: function(item) {
                 item.on('hover:enter', function () {
-                    Lampa.SettingsApi.addParam({
-                        component: 'abros_style',
-                        param: {
-                            name: 'abros_style_info',
-                            type: 'title'
-                        },
-                        field: {
-                            name:'Важно!<br>Перед применением темы отключите предыдущую.'
-                        }
-                    });
+                    $('div[data-component="abros_style_info"]').remove();
                     Lampa.Settings.create('abros_style');
                     Lampa.Controller.enabled().controller.back = function(){
                         Lampa.Settings.create('abros');
@@ -418,7 +409,16 @@ function showReload(reloadText){
                     component: 'abros_style',
                     name: 'Темы'
                 });
-                $('div[data-component="abros_style_info"]').remove();
+                Lampa.SettingsApi.addParam({
+                    component: 'abros_style',
+                    param: {
+                        name: 'abros_style_info',
+                        type: 'title'
+                    },
+                    field: {
+                        name:'Важно!<br>Перед применением темы отключите предыдущую.'
+                    }
+                });
             }
         });
 

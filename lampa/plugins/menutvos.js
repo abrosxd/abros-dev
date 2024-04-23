05c0bec3c9a7f25c80e7fd655920fb911abf6d4a
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-04-23 11:07:31 +0200

    Upd

---
+++ lampa/plugins/menutvos.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+
+    /* Загрузка данных */
+    function startTVOS() {
+        $('.head__actions').append($('.head__time'));
+        $('.head__split').remove();
+        $('<style>.head__time.focus, .head__time.hover { background-color: #fff; color: #000; }</style>').appendTo('head');
+    }
+
+    if (window.appready) {
+        startTVOS();
+    } else {
+        Lampa.Listener.follow('app', e => {
+            if (e.type === 'ready') {
+                startTVOS();
+            }
+        });
+    }
+})();

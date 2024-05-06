bf7330fa3623ba40b3c40df1d81f1fb7139ee82b
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-05-06 21:38:04 +0200

    Upd

--- tilda/mods/player.js
+++ tilda/mods/player.js
@@ -173,7 +173,8 @@ const playPause = (e) => {
         if (!product) return;
         let pagination = catalog.querySelector('.t-store__pagination');
         let activePage = pagination ? Number(pagination.dataset.activePage) : 1;
-        let trackNum = productsArr.indexOf(product);
+        // let trackNum = productsArr.indexOf(product);
+        let trackNum = productsArr.indexOf(product) + (activePage - 1) * tracksOnPage;
         trackNum = trackNum <= playlist.length ? trackNum : productsArr.indexOf(product);
         let track = trackNum !== -1 ? trackLink(playlist[trackNum]) : 0;
         if (audio.src !== track) {

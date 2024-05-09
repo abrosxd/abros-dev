4c91514c428242f999517054152753bb107a5951
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-05-09 22:48:45 +0200

    Upd

--- main/js/logic/projects.js
+++ main/js/logic/projects.js
@@ -1,24 +1,26 @@
+// Поведение карточек
 const main = document.getElementById('main');
 const container = document.querySelector('.container');
 const preload = document.querySelector('.preloadbg');
 const projectCards = [];
 let loadedImages = 0;
-
+        
 function loadImage(url, callback) {
   const img = new Image();
-  img.onload = () => {
+  img.onload = function () {
     loadedImages++;
     callback();
   };
   img.src = url;
 }
-
+        
 function checkLoad(projects) {
   loadedImages++;
   if (loadedImages === projects.length * 2) {
     preload.style.opacity = '0';
     preload.style.visibility = 'hidden';
     projectCards.forEach(card => card.style.display = 'block');
+    
     if (window.innerWidth > 768) {
       main.addEventListener('mousemove', handleMouseMove);
     } else {
@@ -29,40 +31,41 @@ function checkLoad(projects) {
     }
   }
 }
-
+        
 function handleMouseMove(e) {
   requestAnimationFrame(() => {
-    const { left, top } = main.getBoundingClientRect();
-    const centerX = e.clientX - left;
-    const centerY = e.clientY - top;
-
+    const rect = main.getBoundingClientRect();
+    const centerX = e.clientX - rect.left;
+    const centerY = e.clientY - rect.top;
+        
     projectCards.forEach(card => {
-      const { left, top, width, height } = card.getBoundingClientRect();
-      const cardX = left + width / 2;
-      const cardY = top + height / 2;
-
+      const cardRect = card.getBoundingClientRect();
+      const cardX = cardRect.left + cardRect.width / 2;
+      const cardY = cardRect.top + cardRect.height / 2;
+        
       const deltaX = centerX - cardX;
       const deltaY = centerY - cardY;
-
+        
       const tiltX = deltaX / 20;
       const tiltY = deltaY / 20;
-
+        
       card.style.transform = `rotateX(${-tiltY}deg) rotateY(${tiltX}deg)`;
     });
   });
 }
-
+        
 function handleTouchStart() {
   this.classList.add('mobcenter');
 }
-
+        
 function handleTouchEnd() {
   const card = this;
   setTimeout(() => {
     card.classList.remove('mobcenter');
   }, 500);
 }
-
+       
+// Создание карточек
 fetch('main/txt/projects.json')
 .then(response => response.json())
 .then(data => {
@@ -73,70 +76,102 @@ fetch('main/txt/projects.json')
     card.style.boxShadow = `0 0 10px ${project.outline}`;
     card.id = project.id;
     card.dataset.category = project.category;
-
+        
     const overlay = document.createElement('div');
     overlay.className = 'overlay';
     overlay.style.backgroundImage = `url(${project.overlay})`;
     card.appendChild(overlay);
-
+        
     container.appendChild(card);
     projectCards.push(card);
     loadImage(project.img, () => checkLoad(data.projects));
     loadImage(project.overlay, () => checkLoad(data.projects));
   });
-
+  
+  // Создание Popup
   function openPopup(id) {
     const project = data.projects.find(project => project.id === id);
     const popupContent = document.getElementById('popup-content');
     popupContent.innerHTML = '';
+    const content = project.content[currentLanguage];
+    
+    content.forEach(item => {
+      const type = item.type;
+      const value = item.value;
 
-    project.content[currentLanguage].forEach(item => {
-      const { type, value } = item;
-      const element = document.createElement(type === 'code' ? 'pre' : type);
-      if (type === 'img') {
-        element.src = value;
-      } else {
-        if (type === 'code') {
-          element.textContent = `<code class="html">${value}</code>`;
-        } else {
-          element.textContent = value;
-        }
+      if (type === 'h4') {
+        const header = document.createElement('h4');
+        header.textContent = value;
+        header.className = 'popuph4';
+        popupContent.appendChild(header);
+      } else if (type === 'img') {
+        const image = document.createElement('img');
+        image.src = value;
+        image.className = 'popupimg';
+        popupContent.appendChild(image);
+      } else if (type === 'text') {
+        const paragraph = document.createElement('p');
+        paragraph.textContent = value;
+        paragraph.className = 'popuptitle';
+        popupContent.appendChild(paragraph);
+      } else if (type === 'code') {
+        const preformatted = document.createElement('pre');
+        const code = document.createElement('code');
+        code.textContent = value;
+        code.className = 'html';
+        preformatted.className = 'popupcode';
+        preformatted.appendChild(code);
+        popupContent.appendChild(preformatted);
       }
-      element.className = type === 'code' ? 'popupcode' : `popup${type}`;
-      popupContent.appendChild(element);
     });
 
     const popupMenu = document.getElementById('popup-menu');
-    const buttonText = project.url && project.url.trim() !== '' ? project.buttonText[currentLanguage] : '';
-    popupMenu.innerHTML = `
-      ${project.url && project.url.trim() !== '' ? `<a class="button" href="${project.url}" target="_blank">${buttonText}</a>` : ''}
-      <span class="button close-btn" id="close-btn" onclick="closePopup()">
-        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20px" viewBox="0,0,256,256">
-          <g fill="#ffffff" fill-rule="nonzero">
-            <g transform="scale(8.53333,8.53333)">
-              <path d="M7,4c-0.25587,0 -0.51203,0.09747 -0.70703,0.29297l-2,2c-0.391,0.391 -0.391,1.02406 0,1.41406l7.29297,7.29297l-7.29297,7.29297c-0.391,0.391 -0.391,1.02406 0,1.41406l2,2c0.391,0.391 1.02406,0.391 1.41406,0l7.29297,-7.29297l7.29297,7.29297c0.39,0.391 1.02406,0.391 1.41406,0l2,-2c0.391,-0.391 0.391,-1.02406 0,-1.41406l-7.29297,-7.29297l7.29297,-7.29297c0.391,-0.39 0.391,-1.02406 0,-1.41406l-2,-2c-0.391,-0.391 -1.02406,-0.391 -1.41406,0l-7.29297,7.29297l-7.29297,-7.29297c-0.1955,-0.1955 -0.45116,-0.29297 -0.70703,-0.29297z"></path>
+    if (project.url && project.url.trim() !== '') {
+      popupMenu.innerHTML = `
+        <a class="button" href="${project.url}" target="_blank">${project.buttonText[currentLanguage]}</a>
+        <span class="button close-btn" id="close-btn" onclick="closePopup()">
+          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20px" viewBox="0,0,256,256">
+            <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+              <g transform="scale(8.53333,8.53333)">
+                <path d="M7,4c-0.25587,0 -0.51203,0.09747 -0.70703,0.29297l-2,2c-0.391,0.391 -0.391,1.02406 0,1.41406l7.29297,7.29297l-7.29297,7.29297c-0.391,0.391 -0.391,1.02406 0,1.41406l2,2c0.391,0.391 1.02406,0.391 1.41406,0l7.29297,-7.29297l7.29297,7.29297c0.39,0.391 1.02406,0.391 1.41406,0l2,-2c0.391,-0.391 0.391,-1.02406 0,-1.41406l-7.29297,-7.29297l7.29297,-7.29297c0.391,-0.39 0.391,-1.02406 0,-1.41406l-2,-2c-0.391,-0.391 -1.02406,-0.391 -1.41406,0l-7.29297,7.29297l-7.29297,-7.29297c-0.1955,-0.1955 -0.45116,-0.29297 -0.70703,-0.29297z"></path>
+              </g>
             </g>
-          </g>
-        </svg>
-      </span>
-    `;
-
+           </svg>
+        </span>
+      `;
+    } else {
+      popupMenu.innerHTML = `
+        <span class="button close-btn" id="close-btn" onclick="closePopup()">
+          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20px" viewBox="0,0,256,256">
+            <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+              <g transform="scale(8.53333,8.53333)">
+                <path d="M7,4c-0.25587,0 -0.51203,0.09747 -0.70703,0.29297l-2,2c-0.391,0.391 -0.391,1.02406 0,1.41406l7.29297,7.29297l-7.29297,7.29297c-0.391,0.391 -0.391,1.02406 0,1.41406l2,2c0.391,0.391 1.02406,0.391 1.41406,0l7.29297,-7.29297l7.29297,7.29297c0.39,0.391 1.02406,0.391 1.41406,0l2,-2c0.391,-0.391 0.391,-1.02406 0,-1.41406l-7.29297,-7.29297l7.29297,-7.29297c0.391,-0.39 0.391,-1.02406 0,-1.41406l-2,-2c-0.391,-0.391 -1.02406,-0.391 -1.41406,0l-7.29297,7.29297l-7.29297,-7.29297c-0.1955,-0.1955 -0.45116,-0.29297 -0.70703,-0.29297z"></path>
+              </g>
+            </g>
+           </svg>
+        </span>
+      `;
+    }
     const overlay = document.getElementById('overlay-popup');
     const popup = document.getElementById('popup');
+          
     overlay.style.opacity = 1;
     overlay.style.visibility = 'visible';
     popup.style.opacity = 1;
     popup.style.visibility = 'visible';
     popupMenu.style.opacity = 1;
     popupMenu.style.visibility = 'visible';
+
     popup.scrollTop = 0;
+
     window.location.hash = `#${project.hash}`;
     hljs.highlightAll();
   }
-
-  document.querySelectorAll('.project-card').forEach(card => {
+          
+  const projectCard = document.querySelectorAll('.project-card');
+  projectCard.forEach(card => {
     card.addEventListener('click', () => {
-      const projectId = parseInt(card.id);
+      const projectId = parseInt(card.id) ;
       openPopup(projectId);
     });
   });
@@ -145,54 +180,50 @@ fetch('main/txt/projects.json')
     const hash = window.location.hash.substr(1);
     if (hash) {
       const project = data.projects.find(project => project.hash === hash);
-      if (project) openPopup(project.id);
+      if (project) {
+        openPopup(project.id);
+      }
     }
   });
-
-  const changefilters = language => {
-    const { filters } = data;
-    const filterElements = [
-      { element: '.tFilterAll', key: 'All' },
-      { element: '.tFilterMyProject', key: 'MyProject' },
-      { element: '.tFilterLayout', key: 'Layout' },
-      { element: '.tFilterLibrary', key: 'Library' },
-      { element: '.tFilterDesign', key: 'Design' },
-      { element: '.tFilterTilda', key: 'Tilda' }
-    ];
-    filterElements.forEach(({ element, key }) => {
-      const filterElement = document.querySelector(element);
-      filterElement.textContent = filters[key][language];
+  // Смена языка фильтра
+  function changefilters(language) {
+    const filterElements = document.querySelectorAll('.filter .text');
+    const filterNames = ['All', 'MyProject', 'Layout', 'Library', 'Design', 'Tilda'];
+    filterElements.textContent = data.filters.All[language];
+    filterNames.forEach(name => {
+      const element = document.querySelector(`.tFilter${name}`);
+      element.textContent = data.filters[name][language];
     });
-  };
-
+  }
+  
   changefilters(currentLanguage);
 })
 .catch(error => console.error('Ошибка загрузки файла projects.json', error));
 
-const filterSections = document.querySelectorAll('.filter .filter-section');
+// Работа фильтра
+const filterSubMenu = document.querySelector('.filter-wrap.submenu');
 const filterText = document.querySelector('.filter .text');
 const filterArrow = document.querySelector('.filter .arrow');
-const filterSubMenu = document.querySelector('.filter-wrap.submenu');
 
-const filterProjects = category => {
-  document.querySelectorAll('.project-card').forEach(card => {
-    const categories = card.dataset.category.split(' ');
-    card.style.display = (category === 'All' || categories.includes(category)) ? 'block' : 'none';
-  });
-};
+function filterProjects(category) {
+    const cards = document.querySelectorAll('.project-card');
+    cards.forEach(card => {
+        const categories = card.dataset.category.split(' ');
+        card.style.display = (category === 'All' || categories.includes(category)) ? 'block' : 'none';
+    });
+}
 
-filterSections.forEach(section => {
-  section.addEventListener('click', () => {
-    const category = section.dataset.category;
+function handleSectionClick(event) {
+    const category = event.target.dataset.category;
     filterProjects(category);
-    filterText.textContent = section.textContent;
-  });
+    filterText.textContent = event.target.textContent;
+}
+
+document.querySelector('.filter .filter-section').addEventListener('click', handleSectionClick);
+
+const observer = new MutationObserver(() => {
+    filterArrow.style.transform = filterSubMenu.classList.contains('active') ? 'rotate(180deg)' : 'rotate(0deg)';
 });
-
-const handleActiveChange = () => {
-  filterArrow.style.transform = `rotate(${filterSubMenu.classList.contains('active') ? 180 : 0}deg)`;
-};
-
-const observer = new MutationObserver(handleActiveChange);
 observer.observe(filterSubMenu, { attributes: true });
-handleActiveChange();
+
+

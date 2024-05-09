da7ad4ca7cca5cf90f1144d9992419e28245d231
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-05-09 22:11:50 +0200

    Upd

--- main/js/logic/projects.js
+++ main/js/logic/projects.js
@@ -6,7 +6,7 @@ let loadedImages = 0;
 
 function loadImage(url, callback) {
   const img = new Image();
-  img.onload = () => {
+  img.onload = function () {
     loadedImages++;
     callback();
   };
@@ -16,8 +16,10 @@ function loadImage(url, callback) {
 function checkLoad(projects) {
   loadedImages++;
   if (loadedImages === projects.length * 2) {
-    preload.style.opacity = preload.style.visibility = 'hidden';
+    preload.style.opacity = '0';
+    preload.style.visibility = 'hidden';
     projectCards.forEach(card => card.style.display = 'block');
+    
     if (window.innerWidth > 768) {
       main.addEventListener('mousemove', handleMouseMove);
     } else {
@@ -31,15 +33,21 @@ function checkLoad(projects) {
 
 function handleMouseMove(e) {
   requestAnimationFrame(() => {
-    const { left, top } = main.getBoundingClientRect();
-    const centerX = e.clientX - left;
-    const centerY = e.clientY - top;
+    const rect = main.getBoundingClientRect();
+    const centerX = e.clientX - rect.left;
+    const centerY = e.clientY - rect.top;
+    
     projectCards.forEach(card => {
-      const { left, top, width, height } = card.getBoundingClientRect();
-      const cardX = left + width / 2;
-      const cardY = top + height / 2;
-      const tiltX = (centerX - cardX) / 20;
-      const tiltY = (centerY - cardY) / 20;
+      const cardRect = card.getBoundingClientRect();
+      const cardX = cardRect.left + cardRect.width / 2;
+      const cardY = cardRect.top + cardRect.height / 2;
+      
+      const deltaX = centerX - cardX;
+      const deltaY = centerY - cardY;
+      
+      const tiltX = deltaX / 20;
+      const tiltY = deltaY / 20;
+      
       card.style.transform = `rotateX(${-tiltY}deg) rotateY(${tiltX}deg)`;
     });
   });
@@ -56,133 +64,138 @@ function handleTouchEnd() {
   }, 500);
 }
 
-fetch('main/txt/projects.json')
-  .then(response => response.json())
-  .then(({ projects, filters }) => {
-    // Создание карточек проектов
-    projects.forEach(project => {
-      const card = document.createElement('div');
-      card.className = 'project-card';
-      card.style.backgroundImage = `url(${project.img})`;
-      card.style.boxShadow = `0 0 10px ${project.outline}`;
-      card.id = project.id;
-      card.dataset.category = project.category;
+function openPopup(id) {
+  const project = data.projects.find(project => project.id === id);
+  const popupContent = document.getElementById('popup-content');
+  popupContent.innerHTML = '';
+  const content = project.content[currentLanguage];
+  
+  content.forEach(item => {
+    const type = item.type;
+    const value = item.value;
 
-      const overlay = document.createElement('div');
-      overlay.className = 'overlay';
-      overlay.style.backgroundImage = `url(${project.overlay})`;
-      card.appendChild(overlay);
-
-      container.appendChild(card);
-      projectCards.push(card);
-      loadImage(project.img, () => checkLoad(projects));
-      loadImage(project.overlay, () => checkLoad(projects));
-    });
-
-    // Создание Popup
-    function openPopup(id) {
-      const project = projects.find(p => p.id === id);
-      const popupContent = document.getElementById('popup-content');
-      popupContent.innerHTML = '';
-      const content = project.content[currentLanguage];
-
-      content.forEach(item => {
-        const { type, value } = item;
-        const element = document.createElement(type === 'h4' ? 'h4' : type === 'img' ? 'img' : 'p');
-        if (type === 'code') {
-          const preformatted = document.createElement('pre');
-          const code = document.createElement('code');
-          code.textContent = value;
-          code.className = 'html';
-          preformatted.className = 'popupcode';
-          preformatted.appendChild(code);
-          element.appendChild(preformatted);
-        } else {
-          element.textContent = value;
-          element.className = type === 'h4' ? 'popuph4' : type === 'img' ? 'popupimg' : 'popuptitle';
-        }
-        popupContent.appendChild(element);
-      });
-
-      const popupMenu = document.getElementById('popup-menu');
-      popupMenu.innerHTML = project.url && project.url.trim() !== '' ? `
-        <a class="button" href="${project.url}" target="_blank">${project.buttonText[currentLanguage]}</a>
-      ` : '';
-
-      popupMenu.innerHTML += `
-        <span class="button close-btn" id="close-btn" onclick="closePopup()">
-          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20px" viewBox="0,0,256,256">
-            <g fill="#ffffff" fill-rule="nonzero">
-              <g transform="scale(8.53333)">
-                <path d="M7,4c-0.256,0 -0.512,0.098 -0.707,0.293l-2,2c-0.391,0.391 -0.391,1.024 0,1.414l7.293,7.293l-7.293,7.293c-0.391,0.391 -0.391,1.024 0,1.414l2,2c0.391,0.391 1.024,0.391 1.414,0l7.293,-7.293l7.293,7.293c0.391,0.391 1.024,0.391 1.414,0l2,-2c0.391,-0.391 0.391,-1.024 0,-1.414l-7.293,-7.293l7.293,-7.293c0.391,-0.391 0.391,-1.024 0,-1.414l-2,-2c-0.391,-0.391 -1.024,-0.391 -1.414,0l-7.293,7.293l-7.293,-7.293c-0.391,-0.391 -1.024,-0.391 -1.414,0l-2,2c-0.391,0.391 -0.391,1.024 0,1.414l7.293,7.293l-7.293,7.293c-0.391,0.391 -0.391,1.024 0,1.414l2,2c0.391,0.391 1.024,0.391 1.414,0l7.293,-7.293l7.293,7.293c0.391,0.391 1.024,0.391 1.414,0l2,-2c0.391,-0.391 0.391,-1.024 0,-1.414l-7.293,-7.293l7.293,-7.293c0.391,-0.391 0.391,-1.024 0,-1.414l-2,-2c-0.391,-0.391 -1.024,-0.391 -1.414,0l-7.293,7.293l-7.293,-7.293c-0.256,-0.256 -0.512,-0.293 -0.707,-0.293z"/>
-              </g>
-            </g>
-          </svg>
-        </span>
-      `;
-
-      const overlay = document.getElementById('overlay-popup');
-      const popup = document.getElementById('popup');
-
-      overlay.style.opacity = overlay.style.visibility = popup.style.opacity = popup.style.visibility = popupMenu.style.opacity = popupMenu.style.visibility = '1';
-      popup.scrollTop = 0;
-      window.location.hash = `#${project.hash}`;
-      hljs.highlightAll();
+    if (type === 'h4') {
+      const header = document.createElement('h4');
+      header.textContent = value;
+      header.className = 'popuph4';
+      popupContent.appendChild(header);
+    } else if (type === 'img') {
+      const image = document.createElement('img');
+      image.src = value;
+      image.className = 'popupimg';
+      popupContent.appendChild(image);
+    } else if (type === 'text') {
+      const paragraph = document.createElement('p');
+      paragraph.textContent = value;
+      paragraph.className = 'popuptitle';
+      popupContent.appendChild(paragraph);
+    } else if (type === 'code') {
+      const preformatted = document.createElement('pre');
+      const code = document.createElement('code');
+      code.textContent = value;
+      code.className = 'html';
+      preformatted.className = 'popupcode';
+      preformatted.appendChild(code);
+      popupContent.appendChild(preformatted);
     }
-
-    const projectCard = document.querySelectorAll('.project-card');
-    projectCard.forEach(card => {
-      card.addEventListener('click', () => {
-        const projectId = parseInt(card.id);
-        openPopup(projectId);
-      });
-    });
-
-    window.addEventListener('load', () => {
-      const hash = window.location.hash.substr(1);
-      if (hash) {
-        const project = projects.find(project => project.hash === hash);
-        if (project) openPopup(project.id);
-      }
-    });
-
-    // Смена языка фильтра
-    function changefilters(language) {
-      Object.entries(filters).forEach(([key, value]) => {
-        const FilterElement = document.querySelector(`.filter .${key}`);
-        const text = value[language];
-        FilterElement.textContent = text;
-      });
-    }
-
-    changefilters(currentLanguage);
-  })
-  .catch(error => console.error('Ошибка загрузки файла projects.json', error));
-
-// Работа фильтра
-const filterSections = document.querySelectorAll('.filter .filter-section');
-const filterText = document.querySelector('.filter .text');
-const filterArrow = document.querySelector('.filter .arrow');
-const filterSubMenu = document.querySelector('.filter-wrap.submenu');
-
-function filterProjects(category) {
-  const cards = document.querySelectorAll('.project-card');
-  cards.forEach(card => {
-    const categories = card.dataset.category.split(' ');
-    card.style.display = category === 'All' || categories.includes(category) ? 'block' : 'none';
   });
+
+  const popupMenu = document.getElementById('popup-menu');
+  const buttonText = project.url && project.url.trim() !== '' ? project.buttonText[currentLanguage] : '';
+  const buttonHtml = `<a class="button" href="${project.url}" target="_blank">${buttonText}</a>`;
+  const closeHtml = `<span class="button close-btn" id="close-btn" onclick="closePopup()">
+                      <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20px" viewBox="0,0,256,256">
+                        <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+                          <g transform="scale(8.53333,8.53333)">
+                            <path d="M7,4c-0.25587,0 -0.51203,0.09747 -0.70703,0.29297l-2,2c-0.391,0.391 -0.391,1.02406 0,1.41406l7.29297,7.29297l-7.29297,7.29297c-0.391,0.391 -0.391,1.02406 0,1.41406l2,2c0.391,0.391 1.02406,0.391 1.41406,0l7.29297,-7.29297l7.29297,7.29297c0.39,0.391 1.02406,0.391 1.41406,0l2,-2c0.391,-0.391 0.391,-1.02406 0,-1.41406l-7.29297,-7.29297l7.29297,-7.29297c0.391,-0.39 0.391,-1.02406 0,-1.41406l-2,-2c-0.391,-0.391 -1.02406,-0.391 -1.41406,0l-7.29297,7.29297l-7.29297,-7.29297c-0.1955,-0.1955 -0.45116,-0.29297 -0.70703,-0.29297z"></path>
+                          </g>
+                        </g>
+                      </svg>
+                    </span>`;
+  popupMenu.innerHTML = `${buttonHtml}${closeHtml}`;
+  
+  const overlay = document.getElementById('overlay-popup');
+  const popup = document.getElementById('popup');
+  
+  overlay.style.opacity = 1;
+  overlay.style.visibility = 'visible';
+  popup.style.opacity = 1;
+  popup.style.visibility = 'visible';
+  popupMenu.style.opacity = 1;
+  popupMenu.style.visibility = 'visible';
+
+  popup.scrollTop = 0;
+
+  window.location.hash = `#${project.hash}`;
+  hljs.highlightAll();
 }
 
-filterSections.forEach(section => {
-  section.addEventListener('click', () => {
+fetch('main/txt/projects.json')
+.then(response => response.json())
+.then(data => {
+  data.projects.forEach(project => {
+    const card = document.createElement('div');
+    card.className = 'project-card';
+    card.style.backgroundImage = `url(${project.img})`;
+    card.style.boxShadow = `0 0 10px ${project.outline}`;
+    card.id = project.id;
+    card.dataset.category = project.category;
+    
+    const overlay = document.createElement('div');
+    overlay.className = 'overlay';
+    overlay.style.backgroundImage = `url(${project.overlay})`;
+    card.appendChild(overlay);
+    
+    container.appendChild(card);
+    projectCards.push(card);
+    loadImage(project.img, () => checkLoad(data.projects));
+    loadImage(project.overlay, () => checkLoad(data.projects));
+  });
+
+  const projectCard = document.querySelectorAll('.project-card');
+  projectCard.forEach(card => {
+    card.addEventListener('click', () => {
+      const projectId = parseInt(card.id);
+      openPopup(projectId);
+    });
+  });
+
+  window.addEventListener('load', () => {
+    const hash = window.location.hash.substr(1);
+    if (hash) {
+      const project = data.projects.find(project => project.hash === hash);
+      if (project) {
+        openPopup(project.id);
+      }
+    }
+  });
+
+  const changefilters = language => {
+    for (let key in data.filters) {
+      const element = document.querySelector(`.tFilter${key}`);
+      if (element) {
+        element.textContent = data.filters[key][language];
+      }
+    }
+  };
+  changefilters(currentLanguage);
+})
+.catch(error => console.error('Ошибка загрузки файла projects.json', error));
+
+const handleFilterClick = event => {
+  const section = event.target.closest('.filter-section');
+  if (section) {
     const category = section.dataset.category;
     filterProjects(category);
     filterText.textContent = section.textContent;
-  });
-});
+  }
+};
+
+document.querySelector('.filter-wrap.submenu').addEventListener('click', handleFilterClick);
 
 const handleActiveChange = () => {
-  filterArrow.style.transform = filterSubMenu.classList.contains('active') ? 'rotate(180deg)' : 'rotate(0deg)';
+  const rotation = filterSubMenu.classList.contains('active') ? 'rotate(180deg)' : 'rotate(0deg)';
+  filterArrow.style.transform = rotation;
 };
 
 const observer = new MutationObserver(handleActiveChange);

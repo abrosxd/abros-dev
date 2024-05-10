c056cefd6e7be93ab74027aae91f61848d922413
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-05-10 22:46:28 +0200

    Upd

--- main/js/logic/projects.js
+++ main/js/logic/projects.js
@@ -21,7 +21,7 @@ function checkLoad(projects) {
     preload.style.visibility = 'hidden';
     projectCards.forEach(card => card.style.display = 'block');
     
-    if (window.innerWidth > 1024) {
+    if (!('ontouchstart' in window)) {
       main.addEventListener('mousemove', handleMouseMove);
     } else {
       projectCards.forEach(card => {

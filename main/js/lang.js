7b072a79c3737ad7a970835972b14d82b8d1a972
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-05-10 20:52:39 +0200

    Upd

--- main/js/lang.js
+++ main/js/lang.js
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
             setPreviousLanguageFromLocalStorage(currentLanguage)
         }
-    }, 500);
+    }, 1000);
 });
 
 
